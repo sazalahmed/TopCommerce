1656c247e2edc77e740a2ae6a23c620d47b3fafb
sazalahmed <sazal.web@gmail.com>
2025-03-02 16:18:08 +0600

    Six dashboard pages added

--- js/main.js
+++ js/main.js
@@ -878,16 +878,6 @@ $(function () {
     });
 
 
-    // Pregress Bar
-    $(document).ready(function () {
-        $('#bar1').barfiller({});
-        $('#bar2').barfiller({});
-        $('#bar3').barfiller({});
-        $('#bar4').barfiller({});
-        $('#bar5').barfiller({});
-    });
-
-
 });
 
 
